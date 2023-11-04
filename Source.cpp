e7f46b36c6a220e0569b75fe9ce2160da7db52af
hodinhhoang312 <74061056+hodinhhoang312@users.noreply.github.com>
2023-11-04 13:54:54 +0700

    Add files via upload
    Server

---
+++ Source.cpp
@@ -0,0 +1,87 @@
+#include <iostream>
+#include <winsock2.h>
+#include <string>
+#include "Header.h"
+
+#pragma comment(lib, "ws2_32.lib") // Link with the Winsock library
+
+int main() {
+    WSADATA wsaData;
+    if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0) {
+        std::cerr << "Failed to initialize Winsock." << std::endl;
+        return 1;
+    }
+
+    // Create a socket
+    SOCKET serverSocket = socket(AF_INET, SOCK_STREAM, 0);
+    if (serverSocket == INVALID_SOCKET) {
+        std::cerr << "Failed to create a socket." << std::endl;
+        WSACleanup();
+        return 1;
+    }
+
+    sockaddr_in serverAddr;
+    serverAddr.sin_family = AF_INET;
+    serverAddr.sin_addr.s_addr = INADDR_ANY;
+    serverAddr.sin_port = htons(12345); // Use port 12345
+
+    // Bind an address and port to the socket
+    if (bind(serverSocket, (struct sockaddr*)&serverAddr, sizeof(serverAddr)) == SOCKET_ERROR) {
+        std::cerr << "Failed to bind an address and port to the socket." << std::endl;
+        closesocket(serverSocket);
+        WSACleanup();
+        return 1;
+    }
+
+    // Listen for incoming connections
+    if (listen(serverSocket, 5) == SOCKET_ERROR) {
+        std::cerr << "Failed to listen for incoming connections." << std::endl;
+        closesocket(serverSocket);
+        WSACleanup();
+        return 1;
+    }
+
+    std::cout << "Waiting for incoming connections..." << std::endl;
+
+    // Accept a connection
+    SOCKET clientSocket;
+    sockaddr_in clientAddr;
+    int clientAddrLen = sizeof(clientAddr);
+    clientSocket = accept(serverSocket, (struct sockaddr*)&clientAddr, &clientAddrLen);
+    if (clientSocket == INVALID_SOCKET) {
+        std::cerr << "Failed to accept a connection." << std::endl;
+        closesocket(serverSocket);
+        WSACleanup();
+        return 1;
+    }
+
+    std::cout << "Connection established!" << std::endl;
+
+    //Text
+    char buffer[1024];
+
+    while (true) {
+        int bytesRead = recv(clientSocket, buffer, sizeof(buffer), 0);
+        if (bytesRead <= 0) {
+            std::cerr << "Connection closed by client." << std::endl;
+            break;
+        }
+
+        buffer[bytesRead] = '\0';
+        std::cout << "Client: " << buffer << std::endl;
+
+        std::string message;
+        std::cout << "Server: ";
+        std::getline(std::cin, message);
+
+        send(clientSocket, message.c_str(), message.length(), 0);
+    }
+    //
+
+    // Close the sockets
+    closesocket(clientSocket);
+    closesocket(serverSocket);
+    WSACleanup();
+
+    return 0;
+}
