1a3858e6d00287cef911041436a11ae3fac906c0
hodinhhoang312 <74061056+hodinhhoang312@users.noreply.github.com>
2023-11-13 18:13:11 +0700

    Update Capture.h

--- Capture.h
+++ Capture.h
@@ -3,7 +3,7 @@
 #include <SFML/Graphics.hpp>
 #include "Header.h"
 
-const int fps = 240;
+const int fps = 60;
 const int reso = 10;
 int slices = reso;
 
@@ -57,7 +57,7 @@ int sendMatOverSocket(const cv::Mat& image, SOCKET clientSocket) {
     send(clientSocket, (char*)&slices, sizeof(slices), 0);
 
     for (int i = 0; i < slices; ++i) {
-        cv::waitKey(1000 / 500);
+        cv::waitKey(1000 / 100); // Đợi 1/24 giây (của 24 fps)
         int size = partSize + (i == slices - 1 ? remainder : 0); // Phần cuối cùng có thể cần cộng thêm phần dư
 
         int offset = i * partSize;
@@ -143,7 +143,7 @@ cv::Mat receiveMatFromSocket(SOCKET serverSocket) {
         std::cerr << "Error: Incomplete image data received! Received size = " << receivedImageData << " ; Size = " << size << "\n";
 
         if (receivedImageData != size) {
-            std::cerr << "Error: Incomplete image data received! Received size = " << receivedImageData << " ; Size = " << size << "\n";
+            std::cerr << "Error: Incomplete image data received! Received size = "<<receivedImageData<<" ; Size = " << size <<"\n";
             // Xử lý lỗi khi không nhận được đủ dữ liệu ảnh mong đợi
             return cv::Mat(); // Trả về một Mat rỗng để biểu thị lỗi
         }
@@ -201,7 +201,7 @@ int Recv_Screen(SOCKET serverSocket)
         receivedImage = receiveMatFromSocket(serverSocket);
 
         std::cerr << "Image Received!\n";
-        cv::waitKey(1000 / fps);
+        cv::waitKey(1000 / fps); // Đợi 1/24 giây (của 24 fps)
 
         sf::Image image = matToImage(receivedImage);        std::cerr << 2 << '\n';
         sf::Texture texture = imageToTexture(image);        std::cerr << 3 << '\n';
