44e3bf7ad747a7aa8137c1fbdae714d01bf6c785
hodinhhoang312 <74061056+hodinhhoang312@users.noreply.github.com>
2023-11-14 15:36:50 +0700

    Update Capture.h

--- Capture.h
+++ Capture.h
@@ -3,66 +3,93 @@
 #include <SFML/Graphics.hpp>
 #include "Header.h"
 
-const int fps = 12;
-const int reso = 2;
-int slices = 5;
+const int reso = 5;
+int resize = 80;
 
 std::vector<uchar> buf;
 std::vector<int> params;
 
 cv::Mat Capture_Screen() {
-    int width = GetSystemMetrics(SM_CXSCREEN);
-    int height = GetSystemMetrics(SM_CYSCREEN);
+    // Lấy handle của màn hình chính
+    HWND hDesktop = GetDesktopWindow();
 
-    HDC hScreen = GetDC(NULL);
-    HDC hMemoryDC = CreateCompatibleDC(hScreen);
-    HBITMAP hBitmap = CreateCompatibleBitmap(hScreen, width, height);
-    HBITMAP hOldBitmap = (HBITMAP)SelectObject(hMemoryDC, hBitmap);
+    // Lấy thông tin về màn hình
+    RECT desktopRect;
+    GetClientRect(hDesktop, &desktopRect);
+    int width = desktopRect.right;
+    int height = desktopRect.bottom;
 
-    BitBlt(hMemoryDC, 0, 0, width, height, hScreen, 0, 0, SRCCOPY);
+    // Tạo DC (Device Context) cho màn hình
+    HDC hDC = GetDC(hDesktop);
 
-    BITMAPINFOHEADER bi;
-    bi.biSize = sizeof(BITMAPINFOHEADER);
-    bi.biWidth = width;
-    bi.biHeight = -height;  // negative height to ensure top-down DIB
-    bi.biPlanes = 1;
-    bi.biBitCount = 32;     // 32-bit color depth (8 bits for each of B,G,R,A)
-    bi.biCompression = BI_RGB;
-    bi.biSizeImage = 0;
+    // Tạo một bitmap để lưu dữ liệu màn hình
+    HDC hCaptureDC = CreateCompatibleDC(hDC);
+    HBITMAP hBitmap = CreateCompatibleBitmap(hDC, width, height);
+    SelectObject(hCaptureDC, hBitmap);
 
-    cv::Mat screenshot(height, width, CV_8UC4); // 8-bit per channel (32-bit)
-    GetDIBits(hMemoryDC, hBitmap, 0, height, screenshot.data, (BITMAPINFO*)&bi, DIB_RGB_COLORS);
+    // Chụp màn hình và sao chép dữ liệu vào bitmap
+    BitBlt(hCaptureDC, 0, 0, width, height, hDC, 0, 0, SRCCOPY);
 
-    // Release resources
-    SelectObject(hMemoryDC, hOldBitmap);
+    // Tạo đối tượng Mat từ dữ liệu bitmap
+    BITMAPINFOHEADER bmpInfoHeader;
+    bmpInfoHeader.biSize = sizeof(BITMAPINFOHEADER);
+    bmpInfoHeader.biWidth = width;
+    bmpInfoHeader.biHeight = -height; // Negative height to ensure correct order
+    bmpInfoHeader.biPlanes = 1;
+    bmpInfoHeader.biBitCount = 24; // 24-bit RGB
+    bmpInfoHeader.biCompression = BI_RGB;
+    bmpInfoHeader.biSizeImage = 0;
+
+    // Tạo Mat để lưu trữ dữ liệu hình ảnh
+    cv::Mat screenshot(height, width, CV_8UC3);
+    GetDIBits(hDC, hBitmap, 0, height, screenshot.data, (BITMAPINFO*)&bmpInfoHeader, DIB_RGB_COLORS);
+
+    // Giải phóng bộ nhớ
     DeleteObject(hBitmap);
-    DeleteDC(hMemoryDC);
-    ReleaseDC(NULL, hScreen);
+    DeleteDC(hCaptureDC);
+    ReleaseDC(hDesktop, hDC);
 
     return screenshot;
 }
 
-int sendMatOverSocket(const cv::Mat& image, SOCKET clientSocket) {
-    // Chuyển đổi ma trận hình ảnh thành mảng byte để gửi đi
-    std::vector<uchar> buffer;
-    cv::imencode(".jpg", image, buffer);
-
-    // Gửi kích thước của dữ liệu hình ảnh
-    int imgSize = buffer.size();
-    if (send(clientSocket, reinterpret_cast<char*>(&imgSize), sizeof(imgSize), 0) == SOCKET_ERROR) {
-        // Xử lý lỗi khi gửi kích thước dữ liệu
-        return -1;
+void reduceSize(cv::Mat& image, float percent) {
+    // Kiểm tra xem hình ảnh có trống không
+    if (image.empty()) {
+        std::cout << "Hình ảnh trống!" << std::endl;
+        return;
     }
 
-    // Gửi dữ liệu hình ảnh qua socket
-    int bytesSent = send(clientSocket, reinterpret_cast<char*>(buffer.data()), imgSize, 0);
-    if (bytesSent == SOCKET_ERROR || bytesSent != imgSize) {
-        // Xử lý lỗi khi gửi dữ liệu hình ảnh
-        return -1;
-    }
+    // Tính toán kích thước mới dựa trên tỷ lệ phần trăm
+    int newWidth = static_cast<int>(image.cols * (percent / 100));
+    int newHeight = static_cast<int>(image.rows * (percent / 100));
 
-    return 0; // Gửi thành công
+    // Thay đổi kích thước hình ảnh
+    cv::Mat resizedImage;
+    cv::resize(image, resizedImage, cv::Size(newWidth, newHeight));
+
+    // Gán hình ảnh đã thay đổi vào tham số đầu vào
+    image = resizedImage;
 }
+
+
+int sendMatOverSocket(const cv::Mat& image, SOCKET clientSocket) {
+    std::vector<uchar> buf;
+    std::vector<int> params = { cv::IMWRITE_JPEG_QUALITY, reso };
+    cv::imencode("screen.jpg", image, buf, params);
+
+    int totalSize = buf.size();
+
+    // Gửi kích thước dữ liệu
+    if (send(clientSocket, (char*)&totalSize, sizeof(totalSize), 0) == SOCKET_ERROR) {
+        return -1; // Xử lý lỗi khi gửi kích thước dữ liệu
+    }
+
+    // Gửi toàn bộ dữ liệu ảnh trong một gói
+    int bytesSent = send(clientSocket, reinterpret_cast<char*>(buf.data()), totalSize, 0);
+
+    return bytesSent;
+}
+
 
 std::string matToString(const cv::Mat& image) {
     std::stringstream ss;
@@ -76,6 +103,8 @@ int Send_Screen(SOCKET clientSocket)
     while (1)
     {
         cv::Mat screenshot = Capture_Screen(); // Chup man hinh
+
+        reduceSize(screenshot, resize);
 
         if (!screenshot.empty()) {
 
@@ -91,29 +120,45 @@ int Send_Screen(SOCKET clientSocket)
         else {
             std::cerr << "Error: Could not read the image." << std::endl;
         }
-        std::this_thread::sleep_for(std::chrono::milliseconds(1000 / fps));
+        //std::this_thread::sleep_for(std::chrono::milliseconds(1000 / fps));
     }
     return 0;
 }
 
 cv::Mat receiveMatFromSocket(SOCKET serverSocket) {
-    int imgSize = 0;
-    if (recv(serverSocket, reinterpret_cast<char*>(&imgSize), sizeof(imgSize), 0) == SOCKET_ERROR) {
-        // Xử lý lỗi khi nhận kích thước dữ liệu hình ảnh
+    int totalSize = 0;
+
+    // Nhận kích thước dữ liệu
+    if (recv(serverSocket, (char*)&totalSize, sizeof(totalSize), 0) == SOCKET_ERROR) {
+        // Xử lý lỗi khi nhận kích thước dữ liệu
         return cv::Mat(); // Trả về một ma trận rỗng
     }
 
-    std::vector<uchar> buffer(imgSize);
-    int bytesReceived = recv(serverSocket, reinterpret_cast<char*>(buffer.data()), imgSize, 0);
-    if (bytesReceived == SOCKET_ERROR || bytesReceived != imgSize) {
-        // Xử lý lỗi khi nhận dữ liệu hình ảnh
-        return cv::Mat(); // Trả về một ma trận rỗng
+    std::vector<uchar> buffer;
+    buffer.reserve(totalSize);
+
+    int bytesReceived = 0;
+    while (bytesReceived < totalSize) {
+        int remaining = totalSize - bytesReceived;
+        int chunkSize = remaining > 4096 ? 4096 : remaining; // Kích thước chunk mỗi lần nhận
+
+        std::vector<uchar> tempBuffer(chunkSize);
+        int received = recv(serverSocket, reinterpret_cast<char*>(tempBuffer.data()), chunkSize, 0);
+
+        if (received <= 0) {
+            // Xử lý lỗi hoặc kết thúc kết nối
+            return cv::Mat(); // Trả về một ma trận rỗng
+        }
+
+        bytesReceived += received;
+        buffer.insert(buffer.end(), tempBuffer.begin(), tempBuffer.begin() + received);
     }
 
     cv::Mat image = cv::imdecode(buffer, cv::IMREAD_COLOR); // Tạo ma trận hình ảnh từ dữ liệu nhận được
 
     return image; // Trả về ma trận hình ảnh đã nhận
 }
+
 
 sf::Image matToImage(const cv::Mat& mat) {
     sf::Image image;
@@ -139,7 +184,7 @@ cv::Mat receivedImage;
 
 int Recv_Screen(SOCKET serverSocket)
 {
-    sf::RenderWindow window(sf::VideoMode(1920, 1030), "Remote Desktop Client"); // Resolution 1920x1080
+    sf::RenderWindow window(sf::VideoMode(1920*resize/100, 1080*resize/100), "Remote Desktop Client"); // Resolution 1920x1080
     while (window.isOpen()) {
         sf::Event event;
         while (window.pollEvent(event)) {
@@ -153,13 +198,13 @@ int Recv_Screen(SOCKET serverSocket)
         receivedImage = receiveMatFromSocket(serverSocket);
 
         std::cerr << "Image Received!\n";
-        cv::waitKey(1000 / fps); // Đợi 1/24 giây (của 24 fps)
+        //cv::waitKey(1000 / fps); // Đợi 1/24 giây (của 24 fps)
 
-        sf::Image image = matToImage(receivedImage);        std::cerr << 2 << '\n';
-        sf::Texture texture = imageToTexture(image);        std::cerr << 3 << '\n';
-        sf::Sprite sprite(texture);                         std::cerr << 4 << '\n';
+        sf::Image image = matToImage(receivedImage);        
+        sf::Texture texture = imageToTexture(image);        
+        sf::Sprite sprite(texture);                        
 
-        window.draw(sprite);                                std::cerr << 5 << '\n';
+        window.draw(sprite);                                
         window.display();
     }
 
