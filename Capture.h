c192d4f05b7d7a0e003f9c0a7d3ffad8b90fd3e6
hodinhhoang312 <74061056+hodinhhoang312@users.noreply.github.com>
2023-12-05 13:03:00 +0700

    Update Capture.h

--- Capture.h
+++ Capture.h
@@ -1,124 +1,78 @@
 #pragma once
-#include <opencv2/opencv.hpp>
 #include <SFML/Graphics.hpp>
-#include <SFML/Network.hpp>
-#include <SFML/System.hpp>
-#include <SFML/Window.hpp>
+#include <thread>
 #include "Header.h"
-#include <cwchar>
+#include "Capture.h"
+#include "Client_Display.h"
+#include "Server_Display.h"
+#include "Tools.h"
 
-const int reso = 30;
-float resize = 80;
+void Main_Display_Screen()
+{
+    sf::RenderWindow window(sf::VideoMode(1920 * resize / 100, 1080 * resize / 100), "Remote Desktop Client"); // Resolution 1920x1080
 
-std::vector<uchar> buf;
-std::vector<int> params;
+    bool isButtonClicked_Server = false;
+    bool isButtonClicked_Client = false;
 
-cv::Mat Capture_Screen() {
-    // Lấy handle của màn hình chính
-    HWND hDesktop = GetDesktopWindow();
+    bool Server_On = false;
+    bool Server_Connected = false;
+    bool Client_On = false;
+    bool Client_Connected = false;
+    bool Have_Port = false;
 
-    // Lấy thông tin về màn hình
-    RECT desktopRect;
-    GetClientRect(hDesktop, &desktopRect);
-    int width = desktopRect.right;
-    int height = desktopRect.bottom;
+    while (window.isOpen()) {
+     //   std::cerr << "Start!\n";
+      //  std::cerr << Client_On << ' ' << isButtonClicked_Client << '\n';
 
-    // Tạo DC (Device Context) cho màn hình
-    HDC hDC = GetDC(hDesktop);
+        window.clear();
 
-    // Tạo một bitmap để lưu dữ liệu màn hình
-    HDC hCaptureDC = CreateCompatibleDC(hDC);
-    HBITMAP hBitmap = CreateCompatibleBitmap(hDC, width, height);
-    SelectObject(hCaptureDC, hBitmap);
+        Draw_Background(window, "Resources/Image/background.png");
 
-    // Chụp màn hình và sao chép dữ liệu vào bitmap
-    BitBlt(hCaptureDC, 0, 0, width, height, hDC, 0, 0, SRCCOPY);
+        sf::Event event = Get_Event(window);
 
-    // Tạo đối tượng Mat từ dữ liệu bitmap
-    BITMAPINFOHEADER bmpInfoHeader;
-    bmpInfoHeader.biSize = sizeof(BITMAPINFOHEADER);
-    bmpInfoHeader.biWidth = width;
-    bmpInfoHeader.biHeight = -height; // Negative height to ensure correct order
-    bmpInfoHeader.biPlanes = 1;
-    bmpInfoHeader.biBitCount = 24; // 24-bit RGB
-    bmpInfoHeader.biCompression = BI_RGB;
-    bmpInfoHeader.biSizeImage = 0;
+        Draw_Button(window, event, isButtonClicked_Server, 768 + 50, 432 - 100 - 10);
+        Draw_Button(window, event, isButtonClicked_Client, 768 + 50, 432 + 100 - 10);
 
-    // Tạo Mat để lưu trữ dữ liệu hình ảnh
-    cv::Mat screenshot(height, width, CV_8UC3);
-    GetDIBits(hDC, hBitmap, 0, height, screenshot.data, (BITMAPINFO*)&bmpInfoHeader, DIB_RGB_COLORS);
+        Draw_Text(window, "SERVER", 24, sf::Color::White, 768 + 75, 432-100 );
 
-    // Giải phóng bộ nhớ
-    DeleteObject(hBitmap);
-    DeleteDC(hCaptureDC);
-    ReleaseDC(hDesktop, hDC);
+        Draw_Text(window, "CLIENT", 24, sf::Color::White, 768 + 90, 432+100 );
 
-    return screenshot;
-}
+        if (isButtonClicked_Server)
+            Server_On = true;
 
-void reduceSize(cv::Mat& image, float percent) {
-    // Kiểm tra xem hình ảnh có trống không
-    if (image.empty()) {
-        std::cout << "Hình ảnh trống!" << std::endl;
-        return;
-    }
-
-    // Tính toán kích thước mới dựa trên tỷ lệ phần trăm
-    int newWidth = static_cast<int>(image.cols * (percent / 100));
-    int newHeight = static_cast<int>(image.rows * (percent / 100));
-
-    // Thay đổi kích thước hình ảnh
-    cv::Mat resizedImage;
-    cv::resize(image, resizedImage, cv::Size(newWidth, newHeight));
-
-    // Gán hình ảnh đã thay đổi vào tham số đầu vào
-    image = resizedImage;
-}
-
-
-int sendMatOverSocket(const cv::Mat& image, SOCKET clientSocket) {
-    std::vector<uchar> buf;
-    std::vector<int> params = { cv::IMWRITE_JPEG_QUALITY, reso };
-    cv::imencode("screen.jpg", image, buf, params);
-
-    int totalSize = buf.size();
-
-    // Gửi kích thước dữ liệu
-    if (send(clientSocket, (char*)&totalSize, sizeof(totalSize), 0) == SOCKET_ERROR) {
-        return -1; // Xử lý lỗi khi gửi kích thước dữ liệu
-    }
-
-    // Gửi toàn bộ dữ liệu ảnh trong một gói
-    int bytesSent = send(clientSocket, reinterpret_cast<char*>(buf.data()), totalSize, 0);
-
-    return bytesSent;
-}
-
-
-std::string matToString(const cv::Mat& image) {
-    std::stringstream ss;
-    ss << image; // Chuyển đổi Mat thành chuỗi
-
-    return ss.str();
-}
-
-
-sf::Image matToImage(const cv::Mat& mat) {
-    sf::Image image;
-    image.create(mat.cols, mat.rows, sf::Color::Black);
-
-    for (int y = 0; y < mat.rows; y++) {
-        for (int x = 0; x < mat.cols; x++) {
-            cv::Vec3b color = mat.at<cv::Vec3b>(y, x);
-            image.setPixel(x, y, sf::Color(color[2], color[1], color[0]));
+        if (Server_On)
+        {
+            if (Server_Connected)
+                Server_Display_Screen(window, event, "Server Connected!", Server_On);
+            else
+                Server_Display_Screen(window, event, "Waitting For Connection!...", Server_On);
         }
+        else
+            Have_Port = 0;
+
+        if ( Server_On && !Server_Connected && !Have_Port )
+        {
+            std::thread THREAD_S(Server, std::ref(window), std::ref(Server_Connected));
+            THREAD_S.detach();
+           // Server(window, Server_Connected);
+            Server_On = 1;
+            Have_Port = 1;
+        }
+
+        if (isButtonClicked_Client)
+            Client_On = true;
+
+      //  std::cerr << Client_On << ' ' << isButtonClicked_Client << '\n';
+
+        if (Client_On)
+        {
+            Client_Display_Screen(window, event, Client_On);
+        }
+
+
+        isButtonClicked_Server = false;
+        isButtonClicked_Client = false;
+
+        window.display();
     }
-
-    return image;
-}
-
-sf::Texture imageToTexture(const sf::Image& image) {
-    sf::Texture texture;
-    texture.loadFromImage(image);
-    return texture;
 }
